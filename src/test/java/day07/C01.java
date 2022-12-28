73b7f3f57c54f84a9043b83e3189111286d8c1ee
Hazel Cebelli <hazelcebel@gmail.com>
2022-12-28 12:48:17 -0800

    12.28.22

--- src/test/java/day07/C01.java
+++ src/test/java/day07/C01.java
@@ -23,12 +23,12 @@ public class C01 {
             WebDriverManager.chromedriver().setup();
             driver= new ChromeDriver();
             driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
-            driver.manage().window().maximize();
-        }
+            driver.manage().window().maximize();}
         @After
         public void teardown(){
             driver.close();
         }
+
         @Test
         public void test01() throws InterruptedException {
             // ilgili ayarlari yapip
@@ -37,29 +37,30 @@ public class C01 {
             // arama kutusuna java yazdirip aramayi yapin
             // title'in java icerdigini test edin
             driver.get("https://www.amazon.com");
-            // Dropdown menuden istedigimiz option'i secebilmek icin
-            // oncelikle Select class'indan bir obje olusturmaliyiz
-            // ancak select objesi olusturmak icin Select class'inin constructor'i
-            // handle edecegimiz webelemnt'i istediginden
-            // 1 - select objesi olusturmadan once dropdown webelemntini locate etmeliyiz
+            /*
+             Dropdown menuden istedigimiz option'i secebilmek icin oncelikle Select class'indan bir obje olusturmaliyiz. ancak select objesi olusturmak icin Select class'inin constructor'i
+             handle edecegimiz webelemnt'i istediginden select objesi olusturmadan once dropdown webelemntini locate etmeliyiz
+             */
+
             WebElement dropdownWebElementi= driver.findElement(By.xpath("//select[@id='searchDropdownBox']"));
+
             // 2- Select class'indan obje olusturmak
             Select select = new Select(dropdownWebElementi);
+
             // 3- select objesini kullanarak istedigimiz method/method'lari calistirin
             // select.selectByValue("search-alias=stripbooks-intl-ship");
             // select.selectByIndex(5);
             select.selectByVisibleText("Books");
             WebElement aramaKutusu= driver.findElement(By.id("twotabsearchtextbox"));
             aramaKutusu.sendKeys("Java"+ Keys.ENTER);
+
             String expectedKelime="Java";
             String actualTitle= driver.getTitle();
             Assert.assertTrue(actualTitle.contains(expectedKelime));
             // dropdown menuden Books seceneginin secildigini test edin
         /*
-           Locate ettigimiz elementi bulamazsa NoSuchElementException
-           sayfa yenilendigi icin var olan bir elementi kullanamazsa
-           StaleElementException verir
-           bu durumda locate ve secme islemini yeniden yaparsak kodumuz calisir
+           Locate ettigimiz elementi bulamazsa NoSuchElementException  sayfa yenilendigi icin var olan bir elementi kullanamazsa
+           StaleElementException verir  bu durumda locate ve secme islemini yeniden yaparsak kodumuz calisir
          */
             dropdownWebElementi= driver.findElement(By.xpath("//select[@id='searchDropdownBox']"));
             select = new Select(dropdownWebElementi);
@@ -67,6 +68,7 @@ public class C01 {
             String actualSecilenOption = select.getFirstSelectedOption().getText();
             String expectedSecilecekOption="Books";
             Assert.assertEquals(expectedSecilecekOption,actualSecilenOption);
+
             // Dropdown menudeki secenek sayisinin 28 oldugunu test edin
             List<WebElement> optionsWebElementListesi =select.getOptions();
             int actualOptionSayisi = optionsWebElementListesi.size();
--- src/test/java/day07/C02.java
+++ src/test/java/day07/C02.java
@@ -10,40 +10,63 @@ import java.time.Duration;
 
 public class C02 {
 
-   static WebDriver driver;
-
-   @BeforeClass
-    public static void setUp() throws InterruptedException {
-
+    // Gerekli ayarlamalari yapip
+    // https://the-internet.herokuapp.com/javascript_alerts  adresine gidin
+    // 3 test method'u olusturup her method'da bir JsAlert'e basin
+    // Ilgili method'lari kullanin
+    static WebDriver driver;
+    @BeforeClass
+    public static void setUp(){
         WebDriverManager.chromedriver().setup();
         driver= new ChromeDriver();
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
         driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
     }
     @AfterClass
-    public static void teardown(){
+    public static void teardown() throws InterruptedException {
+        Thread.sleep(3000);
         driver.close();
     }
-
-        /*Gerekli ayarlamaları yapıp
-    https://the-internet.herokuapp.com/javascript_alerts adresine gidin
-
-    3 test methodu olusturup her method'da bir JsAlert'e basın
-    ilgili method'ları kullanın
-     */
-
-    @Test   //TEST METHODLARI STATIC OLMAZ
-    public void Alert01(){
-    driver.get("https://the-internet.herokuapp.com/javascript_alerts");
-    driver.findElement(By.xpath("//*[text()=‘Click for JS Alert’]")).click();
-    }
-
     @Test
-    public void Alert02(){
-
+    public void test01() throws InterruptedException {
+        driver.get("https://the-internet.herokuapp.com/javascript_alerts");
+        // 1.alert'e tiklayalim
+        driver.findElement(By.xpath("//*[text()='Click for JS Alert']")).click();
+        // Alert'deki yazinin "I am a JS Alert" oldugunu test edelim
+        String actualAlertyazisi = driver.switchTo().alert().getText();
+        String expectedAlertYazisi="I am a JS Alert";
+        Thread.sleep(3000);
+        Assert.assertEquals(expectedAlertYazisi,actualAlertyazisi);
+        // OK tusuna basip alert'i kapatalim
+        driver.switchTo().alert().accept();
     }
     @Test
-    public void Alert03(){
+    public void test02() throws InterruptedException {
+        driver.get("https://the-internet.herokuapp.com/javascript_alerts");
+        // 2.alert'e tiklayalim
+        driver.findElement(By.xpath("//*[text()='Click for JS Confirm']")).click();
+        // cancel'a basip, cikan sonuc yazisinin "You clicked: Cancel" oldugunu test edin
+        Thread.sleep(3000);
+        driver.switchTo().alert().dismiss();
+        String actualSonucYazisi=driver.findElement(By.xpath("//*[text()='You clicked: Cancel']"))
+                .getText();
+        String expectedSonucYazisi="You clicked: Cancel";
+        Assert.assertEquals(expectedSonucYazisi,actualSonucYazisi);
+    }
+    @Test
+    public void test03() throws InterruptedException {
+        driver.get("https://the-internet.herokuapp.com/javascript_alerts");
+        // 3.alert'e tiklayalim
+        driver.findElement(By.xpath("//*[text()='Click for JS Prompt']")).click();
+        // Cikan prompt ekranina "Abdullah" yazdiralim ve OK tusuna basarak alert'i kapatalim
+        driver.switchTo().alert().sendKeys("Abdullah");
+        Thread.sleep(3000);
+        driver.switchTo().alert().accept();
+        // Cikan sonuc yazisinin Abdullah icerdigini test edelim
+        String actualSonucYazisi=driver.findElement(By.xpath("//*[@id='result']"))
+                .getText();
+        String expectedKelime= "Abdullah";
+        Assert.assertTrue(actualSonucYazisi.contains(expectedKelime));
 
     }
 }
