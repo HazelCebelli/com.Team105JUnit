cbb9976fa1f7b7165ba51d707cab07b3497082dd
Hazel Cebelli <hazelcebel@gmail.com>
2022-12-30 13:49:25 -0800

    12.30.22

--- src/test/java/day10/C01_File_Olusturma.java
+++ src/test/java/day10/C01_File_Olusturma.java
@@ -1,11 +1,27 @@
 package day10;
 
+import org.junit.Test;
 import utilities.TestBase;
 
-public class C01_File_Olusturma extends TestBase {
+import java.awt.*;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
+public class C01_File_Olusturma {
+
+    @Test
+    public void test01() throws FileNotFoundException {
+
+    String filePath="\"C:\\Users\\cebel\\OneDrive\\Masaüstü\\MerhabaJava\"";
+    FileInputStream fis=new FileInputStream(filePath);
+
+    System.out.println(System.getProperty("user.dir"));
 
 
+    System.out.println(System.getProperty("user.home"));
+
+   // String dinamikDosyaYolu=System.getProperty("user.home")+"/Desctop.....";
 
 
-
+}
 }
---
+++ src/test/java/day10/C02.java
@@ -0,0 +1,10 @@
+package day10;
+
+public class C02 {
+
+
+//   1. Tests packagenin altina bir class oluşturalim : C04_FileDownload
+//2. https://the-internet.herokuapp.com/download adresine gidelim.
+//3. logo.png dosyasını indirelim
+//4. Dosyanın başarıyla indirilip indirilmediğini test edelim
+}
